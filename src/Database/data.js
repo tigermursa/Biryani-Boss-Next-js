f96179e38645546c6a58128e3f0584b957bbedf2
tigermursa <mursalin2311@gmail.com>
2024-05-19 01:32:46 +0600

    "updated product images"

--- src/Database/data.js
+++ src/Database/data.js
@@ -4,7 +4,7 @@ export const data = [
     name: "Chicken Biryani",
     description:
       "Succulent chicken and aromatic spices layered with fragrant basmati rice.",
-    image: "/products/chicken-biryani.webp",
+    image: "/products/chicken-briyani-hq.webp",
     priceSm: 150,
     priceMd: 420,
     priceLg: 710,
@@ -85,7 +85,7 @@ export const data = [
   },
   {
     id: 103,
-    name: "Kacchi Biryani",
+    name: "Kacchi Biryani (Bashmoti)",
     description:
       "Tender mutton and aromatic spices marry fragrant basmati rice",
     image: "/products/kacchi-biryani.webp",
@@ -126,7 +126,7 @@ export const data = [
     ],
   },
   {
-    id: 101,
+    id: 104,
     name: "Egg Khichuri",
     description:
       "Fluffy rice, lentils, and boiled eggs seasoned with aromatic spices.",
@@ -134,6 +134,342 @@ export const data = [
     priceSm: 100,
     priceMd: 280,
     priceLg: 460,
+    drinks: [
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 250ml",
+        price: 90,
+      },
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 500ml",
+        price: 160,
+      },
+      {
+        image: "/products/mojo-250ml.webp",
+        name: "Mojo 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/mojo-500ml.webp",
+        name: "Mojo 500ml",
+        price: 35,
+      },
+      {
+        image: "/products/pranup-250ml.webp",
+        name: "PranUP 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/cheerup-500ml.webp",
+        name: "PranUP-500ml",
+        price: 30,
+      },
+    ],
+  },
+  {
+    id: 105,
+    name: "Beef Khichuri",
+    description:
+      "Fluffy rice, lentils, and boiled eggs seasoned with aromatic spices.",
+    image: "/products/beef-Khichuri.webp",
+    priceSm: 130,
+    priceMd: 360,
+    priceLg: 600,
+    drinks: [
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 250ml",
+        price: 90,
+      },
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 500ml",
+        price: 160,
+      },
+      {
+        image: "/products/mojo-250ml.webp",
+        name: "Mojo 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/mojo-500ml.webp",
+        name: "Mojo 500ml",
+        price: 35,
+      },
+      {
+        image: "/products/pranup-250ml.webp",
+        name: "PranUP 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/cheerup-500ml.webp",
+        name: "PranUP-500ml",
+        price: 30,
+      },
+    ],
+  },
+  {
+    id: 106,
+    name: "Beef Tehari",
+    description:
+      "Fluffy rice, lentils, and boiled eggs seasoned with aromatic spices.",
+    image: "/products/beef-tehari.webp",
+    priceSm: 150,
+    priceMd: 420,
+    priceLg: 710,
+    drinks: [
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 250ml",
+        price: 90,
+      },
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 500ml",
+        price: 160,
+      },
+      {
+        image: "/products/mojo-250ml.webp",
+        name: "Mojo 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/mojo-500ml.webp",
+        name: "Mojo 500ml",
+        price: 35,
+      },
+      {
+        image: "/products/pranup-250ml.webp",
+        name: "PranUP 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/cheerup-500ml.webp",
+        name: "PranUP-500ml",
+        price: 30,
+      },
+    ],
+  },
+  {
+    id: 107,
+    name: "Chicken Khichuri",
+    description:
+      "Fluffy rice, lentils, and boiled eggs seasoned with aromatic spices.",
+    image: "/products/chicken-khichuri.webp",
+    priceSm: 80,
+    priceMd: 240,
+    priceLg: 380,
+    drinks: [
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 250ml",
+        price: 90,
+      },
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 500ml",
+        price: 160,
+      },
+      {
+        image: "/products/mojo-250ml.webp",
+        name: "Mojo 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/mojo-500ml.webp",
+        name: "Mojo 500ml",
+        price: 35,
+      },
+      {
+        image: "/products/pranup-250ml.webp",
+        name: "PranUP 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/cheerup-500ml.webp",
+        name: "PranUP-500ml",
+        price: 30,
+      },
+    ],
+  },
+  {
+    id: 108,
+    name: "Mutton Khichuri",
+    description:
+      "Fluffy rice, lentils, and boiled eggs seasoned with aromatic spices.",
+    image: "/products/mutton-khichuri.webp",
+    priceSm: 80,
+    priceMd: 240,
+    priceLg: 380,
+    drinks: [
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 250ml",
+        price: 90,
+      },
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 500ml",
+        price: 160,
+      },
+      {
+        image: "/products/mojo-250ml.webp",
+        name: "Mojo 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/mojo-500ml.webp",
+        name: "Mojo 500ml",
+        price: 35,
+      },
+      {
+        image: "/products/pranup-250ml.webp",
+        name: "PranUP 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/cheerup-500ml.webp",
+        name: "PranUP-500ml",
+        price: 30,
+      },
+    ],
+  },
+  {
+    id: 109,
+    name: "Special Chicken Curry",
+    description:
+      "Fluffy rice, lentils, and boiled eggs seasoned with aromatic spices.",
+    image: "/products/chicken.webp",
+    priceSm: 70,
+    priceMd: 140,
+    priceLg: 350,
+    drinks: [
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 250ml",
+        price: 90,
+      },
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 500ml",
+        price: 160,
+      },
+      {
+        image: "/products/mojo-250ml.webp",
+        name: "Mojo 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/mojo-500ml.webp",
+        name: "Mojo 500ml",
+        price: 35,
+      },
+      {
+        image: "/products/pranup-250ml.webp",
+        name: "PranUP 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/cheerup-500ml.webp",
+        name: "PranUP-500ml",
+        price: 30,
+      },
+    ],
+  },
+  {
+    id: 110,
+    name: "Special Roasted Chicken",
+    description:
+      "Fluffy rice, lentils, and boiled eggs seasoned with aromatic spices.",
+    image: "/products/spatchcocked-roasted-chicken.webp",
+    priceSm: 320,
+    priceMd: 960,
+    priceLg: 1600,
+    drinks: [
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 250ml",
+        price: 90,
+      },
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 500ml",
+        price: 160,
+      },
+      {
+        image: "/products/mojo-250ml.webp",
+        name: "Mojo 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/mojo-500ml.webp",
+        name: "Mojo 500ml",
+        price: 35,
+      },
+      {
+        image: "/products/pranup-250ml.webp",
+        name: "PranUP 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/cheerup-500ml.webp",
+        name: "PranUP-500ml",
+        price: 30,
+      },
+    ],
+  },
+  {
+    id: 111,
+    name: "White Rice Chicken Vegetable",
+    description:
+      "Fluffy rice, lentils, and boiled eggs seasoned with aromatic spices.",
+    image: "/products/white rice with chinken and vagies.webp",
+    priceSm: 150,
+    priceMd: 420,
+    priceLg: 710,
+    drinks: [
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 250ml",
+        price: 90,
+      },
+      {
+        image: "/products/borhani.webp",
+        name: "Borhani 500ml",
+        price: 160,
+      },
+      {
+        image: "/products/mojo-250ml.webp",
+        name: "Mojo 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/mojo-500ml.webp",
+        name: "Mojo 500ml",
+        price: 35,
+      },
+      {
+        image: "/products/pranup-250ml.webp",
+        name: "PranUP 250ml",
+        price: 20,
+      },
+      {
+        image: "/products/cheerup-500ml.webp",
+        name: "PranUP-500ml",
+        price: 30,
+      },
+    ],
+  },
+  {
+    id: 112,
+    name: "White Rice",
+    description:
+      "Fluffy rice, lentils, and boiled eggs seasoned with aromatic spices.",
+    image: "/products/white rice.webp",
+    priceSm: 30,
+    priceMd: 90,
+    priceLg: 150,
     drinks: [
       {
         image: "/products/borhani.webp",
