5ee9b5572bf04b5f3bb11a910245f1cabfc836e0
tigermursa <mursalin2311@gmail.com>
2024-05-20 01:28:29 +0600

    added flag on footer

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -1,7 +1,15 @@
 import Image from "next/image";
 import React from "react";
-import { FaFacebookSquare, FaInstagram, FaWhatsapp } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import {
+  FaCopyright,
+  FaFacebookSquare,
+  FaInstagram,
+  FaRegClock,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { FaPhoneFlip, FaSquareXTwitter } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+import { TbWorld } from "react-icons/tb";
 
 const Footer = () => {
   return (
@@ -17,20 +25,67 @@ const Footer = () => {
       <div className="container mx-auto px-4 py-8 sm:px-6 lg:px-8 bg-black bg-opacity-60 rounded-t-2xl">
         <div className="flex flex-col lg:flex-row items-start lg:items-center justify-between">
           <div className="mb-4 lg:mb-0">
-            <Image src="/logo/logo.png" alt="Logo" width={150} height={150} />
+            <Image src="/logo/logo.png" alt="Logo" width={130} height={130} />
             <div className="flex justify-center text-2xl gap-2 text-white cursor-pointer">
               <FaFacebookSquare />
               <FaInstagram />
               <FaWhatsapp />
-              <FaSquareXTwitter/>
+              <FaSquareXTwitter />
             </div>
           </div>
           <div className="flex flex-col lg:flex-row items-start lg:items-center justify-center text-white text-sm">
             <div className="mb-4 lg:mb-0 lg:mr-12 text-lg font-semibold">
-              <p className="mb-2">Briyani Boss Bangladesh</p>
-              <p className="mb-2">briyanibossbd@info.com</p>
-              <p>(+88) 01856-789086</p>
+              <div className="flex items-center gap-2">
+                <TbWorld />
+                <p>Briyani Boss Bangladesh</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <MdOutlineEmail />
+                <p>briyanibossbd@info.com</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <FaPhoneFlip /> <p>+8801456845564</p>
+              </div>
+              <div className="flex gap-2 mt-2 ">
+                <Image
+                  src={"/flags/bangladesh.png"}
+                  alt="bangladesh"
+                  width={30}
+                  height={30}
+                />
+                <Image
+                  src={"/flags/pakistan.png"}
+                  alt="pakistan"
+                  width={30}
+                  height={30}
+                />
+                <Image
+                  src={"/flags/japan.png"}
+                  alt="japan"
+                  width={30}
+                  height={30}
+                />
+                <Image
+                  src={"/flags/turky.png"}
+                  alt="turky"
+                  width={30}
+                  height={30}
+                />
+                <Image
+                  src={"/flags/ksa.png"}
+                  alt="ksa"
+                  width={30}
+                  height={30}
+                />
+                <Image
+                  src={"/flags/afganistan.png"}
+                  alt="afganistan"
+                  width={30}
+                  height={30}
+                />
+              </div>
             </div>
+
             <div className="text-xl font-semibold">
               <div className="flex flex-wrap gap-10 font-light text-sm">
                 <ul className="text-white">
@@ -76,10 +131,19 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="text-white h-6 mt-8 flex items-center font-semibold text-sm justify-between border-t">
-          <p>Copyright 2024 Biryani Boss</p>
-          <p>Hot Line : +8801456845564</p>
-          <p>10 am to 10 pm</p>
+        {/* mobile */}
+        <div className="text-white h-6 mt-8 flex flex-col md:flex-row gap-1 items-center font-semibold text-sm justify-between border-t">
+          <div className="flex items-center gap-2">
+            <FaCopyright /> <p>Copyright 2024 Biryani Boss</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <FaPhoneFlip /> <p> +8801456845564</p>
+          </div>
+          <div className="hidden md:block">
+            <div className="flex items-center gap-2">
+              <FaRegClock /> <p>10-am to 10-am</p>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
