a310a396aecc53c1cfb790092fb80c2d7555fe96
tigermursa <mursalin2311@gmail.com>
2024-06-05 15:41:01 +0600

    "navigation responsive done"

--- src/Components/Home/Navigation/Navigation.js
+++ src/Components/Home/Navigation/Navigation.js
@@ -1,45 +1,63 @@
-
+"use client"
+import "./Navigation.css"
+import { useState } from "react";
 import Image from "next/image";
-// Icons
-import {
-  FaAlignJustify,
-  FaFacebookSquare,
-  FaInstagram,
-  FaWhatsapp,
-} from "react-icons/fa";
+import { FaAlignJustify, FaFacebookSquare, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 const Navigation = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
-    <nav className=" container mx-auto">
-      <div className="flex  justify-between items-center ">
-        {/* logo */}
+    <nav className="container mx-auto">
+      <div className="flex justify-between items-center">
+        {/* Logo and Social Media Icons */}
         <div className="flex items-center">
-          <div className="  w-[70px]">
+          <div className="w-[70px]">
             <Image width={200} height={200} src={"/logo/logo.png"} alt="logo" />
           </div>
-          {/* social media icons */}
-          <div className="flex text-2xl gap-2 text-red-500 cursor-pointer">
+          <div className="flex text-2xl gap-2 text-red-500 cursor-pointer md:hidden">
             <FaFacebookSquare />
             <FaInstagram />
             <FaWhatsapp />
           </div>
         </div>
 
+        {/* Burger Menu Icon for Mobile */}
         <div className="md:hidden">
-          <FaAlignJustify className="text-black text-3xl mx-5" />
+          <FaAlignJustify
+            className="text-black text-3xl mx-5 cursor-pointer"
+            onClick={toggleDrawer}
+          />
         </div>
-        <div className=" hidden md:flex mx-1 items-center justify-center">
-          <ul className="flex gap-8 mx-10 font-semibold font-serif text-md  items-center cursor-pointer ">
+
+        {/* Navigation Links */}
+        <div className="hidden md:flex mx-1 items-center justify-center">
+          <ul className="flex gap-8 mx-10 font-semibold font-serif text-md items-center cursor-pointer">
             <li>Home</li>
             <li>Menu</li>
             <li>Offers</li>
             <li>Order</li>
             <li>About Us</li>
           </ul>
-          <button className="  btn btn-primary btn-sm gradient primary-font  ">
+          <button className="btn btn-primary btn-sm gradient primary-font">
             Order Now!
           </button>
         </div>
+      </div>
+
+      {/* Drawer for Mobile */}
+      <div className={`drawer ${isDrawerOpen ? 'open' : ''}`}>
+        <ul className="flex flex-col gap-4 mt-4 mx-5">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Offers</li>
+          <li>Order</li>
+          <li>About Us phone</li>
+        </ul>
       </div>
     </nav>
   );
